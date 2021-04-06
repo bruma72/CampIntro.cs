f2fecc0ddfe2d111ed505df70a8fa653a3ab9c00
bruma72 <insua72@hotmail.com>
2021-04-06 21:59:39 +0300

    Add project files.

---
+++ Matematik.cs/DortIslem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matematik.cs
+{
+    class DortIslem
+    {
+        public void Topla(int sayi1, int sayi2)
+        {
+
+            Console.WriteLine($"Sonuç: {sayi1 + sayi2 }");
+
+        }
+    }
+}
---
+++ Metotlar.cs/SepetManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar.cs
+{
+    class SepetManager
+    {
+        public void Ekle(Urun urun)
+        {
+
+            Console.WriteLine("Sepete eklendi :" + " " + urun.Adi);
+
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat)
+        {
+            Console.WriteLine("Sepete eklendi :" + " " + urunAdi);
+        }
+    }
+}
